4ce578841b1855cc61c978fe2b8eeead11598120
Jonathan <Jonathan.cummings1013@gmail.com>
2021-04-03 17:12:53 -0700

    display

--- lib/ghibli/cli.rb
+++ lib/ghibli/cli.rb
@@ -1,3 +1,5 @@
+#No case sensitivty 
+
 class CLI 
     def start
         puts <<-'EOF'
@@ -18,6 +20,7 @@ EOF
     end
     def greet(name)
         puts ""
+        sleep (0.50)
         puts "Wow... #{name}, what a nice name! Would you like to see some of Studio Ghibli's movies today? If so, kindly enter yes to see a list of movies otherwise enter exit to exit".colorize(:light_blue)
         puts ""
         menu
@@ -26,6 +29,7 @@ EOF
         selection = user_input
         if selection == "yes"
             puts ""
+            sleep (1)
             print_films
         elsif selection == "exit"
             goodbye
@@ -62,11 +66,14 @@ EOF
                                                                                                                                  
 EOF
         puts "        Thank you for visiting us!".colorize(:red)
+        sleep (0.50)
         puts "            come back soon".colorize(:red)
+        sleep (0.50)
         puts "                 xoxo".colorize(:red)
     end
     def invalid 
         puts "well...that doesn't seem to look right. Could you try again please? ^_^".colorize(:light_pink)
+        sleep (0.50)
         puts ""
         puts "enter yes to see more movie or exit to exit".colorize(:light_pink)
         menu
@@ -80,6 +87,7 @@ EOF
     end
     def select_films
         puts "Enter name of movie you would like to learn more about please.".colorize(:red)
+        sleep (2)
         puts ""
         selection = user_input 
         if Movies.find_by_selection(selection)
@@ -98,14 +106,20 @@ EOF
         puts ""
         puts "-------------------------------------".colorize(:red)
         puts "Original Title: #{films.original_title}".colorize(:yellow)
+        sleep (1)
         puts "North American Title: #{films.title}".colorize(:green)
+        sleep (1)
         puts "Description: #{films.description}".colorize(:blue)
+        sleep (1)
         puts "Release date: #{films.release_date}".colorize(:red)
+        sleep (1)
         puts "How long: #{films.running_time} minutes".colorize(:yellow)
+        sleep (1)
         puts ""
         puts "-------------------------------------".colorize(:red)
         puts ""
         puts ""
+        sleep (1)
         puts "enter yes to see more movie or exit to exit"
         menu
         else 
